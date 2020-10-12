7280b1b70c6f9745cca3c71a637653b2b093943c
waigo001 <kinwaka01@gmail.com>
2020-10-12 12:47:56 +0900

    Typescriptへ変更

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -6,11 +6,22 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+interface MetaObject {
+  name: string;
+  content: string;
+}
+
+interface Prop {
+  description?: string;
+  lang?: string;
+  meta?: MetaObject[];
+  title: string;
+}
+
+const SEO:React.FC<Prop>=({ description='', lang='ja', meta=[], title })=> {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -73,17 +84,5 @@ function SEO({ description, lang, meta, title }) {
   )
 }
 
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-}
 
 export default SEO
