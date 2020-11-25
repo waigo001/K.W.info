35c002d55f6ddb89c5b3bbdbc56fcd93fbd2f405
waigo001 <kinwaka01@gmail.com>
2020-11-25 18:07:40 +0900

    microCMSを追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,11 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `K.W.info`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `K.W.の日常ブログです。`,
     author: `K.W.`,
   },
   plugins: [
@@ -24,10 +28,23 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, 
       },
     },
     "gatsby-plugin-material-ui",
+    {
+      resolve: "gatsby-source-microcms",
+      options: {
+        apiKey: process.env.MICROCMS_API_KEY, 
+        serviceId: process.env.MICROCMS_ENDPOINT,
+        apis: [
+          {
+            endpoint: "blogs",
+            format: "list",
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
