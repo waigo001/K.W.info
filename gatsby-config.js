a0f580cad9c8961c2fad99379168067527436a27
waigo001 <kinwaka01@gmail.com>
2020-11-26 22:53:31 +0900

    gatsby-plugin-typegen追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,38 +4,38 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `K.W.info`,
-    description: `K.W.の日常ブログです。`,
-    author: `K.W.`,
+    title: "K.W.info",
+    description: "K.W.の日常ブログです。",
+    author: "K.W.",
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `K.W.info`,
-        short_name: `K.W.info`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, 
+        name: "K.W.info",
+        short_name: "K.W.info",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png",
       },
     },
     "gatsby-plugin-material-ui",
     {
       resolve: "gatsby-source-microcms",
       options: {
-        apiKey: process.env.MICROCMS_API_KEY, 
+        apiKey: process.env.MICROCMS_API_KEY,
         serviceId: process.env.MICROCMS_ENDPOINT,
         apis: [
           {
@@ -45,8 +45,16 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: "gatsby-plugin-typegen",
+      options: {
+        emitSchema: {
+          "src/__generated__/gatsby-introspection.json": true,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // "gatsby-plugin-offline",
   ],
 }
