0cb537ad2e38236e91c5037a06b3dd4026d60b82
waigo001 <kinwaka01@gmail.com>
2020-12-15 22:00:45 +0900

    Twitter、共有リンクの作成 Fixes #7

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,25 +1,11 @@
-import {
-  Box,
-  Card,
-  CardActions,
-  CardContent,
-  Container,
-  Grid,
-  IconButton,
-  makeStyles,
-  Typography,
-} from "@material-ui/core"
-import LinkIcon from "@material-ui/icons/Link"
-import TwitterIcon from "@material-ui/icons/Twitter"
-import LocalOfferIcon from "@material-ui/icons/LocalOffer"
+import { Container, Grid, makeStyles } from "@material-ui/core"
 
 import React from "react"
 import Layout from "../components/layout/blog"
 import SEO from "../components/seo"
 import theme from "../styles/theme"
-import { graphql, Link, PageProps } from "gatsby"
-import Tag from "../components/tag"
-import PostTime from "../components/postTime"
+import { graphql, PageProps } from "gatsby"
+import BlogCard from "../components/blogCard"
 
 export const query = graphql`
   query BlogPages {
@@ -37,6 +23,11 @@ export const query = graphql`
         updatedAt
       }
     }
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
   }
 `
 
@@ -44,97 +35,27 @@ const useStyles = makeStyles({
   container: {
     padding: theme.spacing(1),
   },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  tagIcon: {
-    marginRight: theme.spacing(0.5),
-    fontSize: "1rem",
-  },
-  cardTitle: {
-    margin: theme.spacing(0.5),
-    textDecoration: "none",
-    fontSize: "1.5rem",
-    color: theme.palette.text.primary,
-    fontWeight: 700,
-  },
-  cardContent: {
-    flexGrow: 1,
-    paddingBottom: 0,
-  },
-  chip: {
-    margin: theme.spacing(0.5),
-  },
-  iconButton: {
-    marginLeft: "auto",
-  },
 })
 
 const BlogPage: React.FC<PageProps<GatsbyTypes.BlogPagesQuery>> = ({
   data,
 }) => {
   const classes = useStyles()
-  const items = data.allMicrocmsBlogs.nodes.map(step => (
-    <Grid item xs={12} sm={6} md={4} key={step.title}>
-      <Card className={classes.card} elevation={2}>
-        <CardContent className={classes.cardContent}>
-          <Box
-            color="text.secondary"
-            display="flex"
-            alignItems="center"
-            fontSize="small"
-          >
-            <PostTime
-              updatedAt={step.updatedAt}
-              publishedAt={step.publishedAt}
-            />
-          </Box>
-          <Typography
-            className={classes.cardTitle}
-            component={Link}
-            to={`/blog/${step.slug}`}
-          >
-            {step.title}
-          </Typography>
-          <Box
-            color="text.secondary"
-            display="flex"
-            alignItems="center"
-            fontSize="small"
-            marginTop={1}
-            marginBottom={1}
-          >
-            <LocalOfferIcon className={classes.tagIcon} />
-            タグ
-          </Box>
-          <Box>
-            {step.tags !== undefined ? (
-              step.tags.map(key => <Tag props={key} key={key?.title} />)
-            ) : (
-              <></>
-            )}
-          </Box>
-        </CardContent>
-        <CardActions>
-          <IconButton aria-label="twitter" className={classes.iconButton}>
-            <TwitterIcon />
-          </IconButton>
-          <IconButton aria-label="link">
-            <LinkIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
-    </Grid>
-  ))
 
   return (
     <Layout>
       <SEO title="Blog" />
       <Container maxWidth="md" className={classes.container}>
         <Grid container spacing={2}>
-          {items}
+          {data.allMicrocmsBlogs.nodes.map(step => (
+            <Grid item xs={12} sm={6} md={4} key={step.title}>
+              <BlogCard
+                step={step}
+                key={step.slug}
+                url={data.site?.siteMetadata?.siteUrl}
+              />
+            </Grid>
+          ))}
         </Grid>
       </Container>
     </Layout>
