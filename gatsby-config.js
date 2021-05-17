1c48c84d18634e466e72e8eb0510f2eaf902bffc
waigo001 <kinwaka01@gmail.com>
2021-05-17 23:24:34 +0900

    不要パッケージ削除

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,12 +51,6 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
-      options: {
-        devMode: true,
-      },
-    },
-    {
       resolve: "gatsby-plugin-typegen",
       options: {
         emitSchema: {
