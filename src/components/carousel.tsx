10e43e9358facf3cbb05f9d713ab45413e1c7f55
waigo001 <kinwaka01@gmail.com>
2021-05-17 14:44:33 +0900

    npm audit

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,23 +1,6 @@
-import { graphql, Link, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
-import SwipeableViews from "react-swipeable-views"
-import { autoPlay } from "react-swipeable-views-utils"
-import { format, parseISO } from "date-fns"
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews)
-
-const Tag: React.FC<{
-  props:
-    | GatsbyTypes.Maybe<Pick<GatsbyTypes.MicrocmsBlogsTags, "title" | "slug">>
-    | undefined
-}> = ({ props }) => {
-  let contents = <></>
-  if (props !== undefined) {
-    if (props.slug !== undefined && props.title !== undefined)
-      contents = <span>#{props.title}</span>
-  }
-  return contents
-}
 
 const Carousel: React.FC = () => {
   const data = useStaticQuery<GatsbyTypes.BlogsQuery>(graphql`
@@ -38,56 +21,14 @@ const Carousel: React.FC = () => {
     }
   `)
 
-  const [activeStep, setActiveStep] = React.useState(0)
-
   let maxSteps = 3
   if (data.allMicrocmsBlogs.nodes.length < 3)
     maxSteps = data.allMicrocmsBlogs.nodes.length
 
-  const handleNext = () => {
-    setActiveStep(prevActiveStep => prevActiveStep + 1)
-  }
-
-  const handleBack = () => {
-    setActiveStep(prevActiveStep => prevActiveStep - 1)
-  }
-
-  const handleStepChange = (step: number) => {
-    setActiveStep(step)
-  }
 
   return (
-    <div>
-      <AutoPlaySwipeableViews
-        axis="x"
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-        interval={5000}
-      >
-        {data.allMicrocmsBlogs.nodes.map(step => (
-          <div key={step.title}>
-            <div>published at</div>
-            <div>
-              {step.publishedAt !== undefined
-                ? format(parseISO(step.publishedAt), "yyyy/MM/dd")
-                : ""}
-            </div>
-            <div>
-              <Link to={`/blog/${step.slug}`}>{step.title}</Link>
-            </div>
-            <div>tags</div>
-            <div>
-              {step.tags !== undefined ? (
-                step.tags.map(key => <Tag props={key} key={key?.slug} />)
-              ) : (
-                <></>
-              )}
-            </div>
-          </div>
-        ))}
-      </AutoPlaySwipeableViews>
-    </div>
+    <>
+    </>
   )
 }
 
